b8fa9c23bf1507d4873e5ef88bd4e6523b176747
MikeMalindzisa <machaweml@gmail.com>
2023-04-25 19:29:57 -0700

    n

--- convertors/convert_char.c
+++ convertors/convert_char.c
@@ -1,19 +1,19 @@
 #include "../main.h"
 
 /**
-* convert_char - Converts a char to a string
-*
-* @ap: list containing a single character
-*
-* Return: Pointer to the dynamically allocated string
-*/
+ * convert_char - Converts a char to a string
+ *
+ * @ap: list containing a single character
+ *
+ * Return: Pointer to the converted string
+ */
 char *convert_char(va_list ap)
 {
-char *s = malloc(2 * sizeof(char));
+    static char s[2];
 
-if (!s)
-return (NULL);
-s[0] = va_arg(ap, int);
-s[1] = '\0';
-return (s);
+    s[0] = va_arg(ap, int);
+    s[1] = '\0';
+
+    return (s);
 }
+
--- main.c
+++ main.c
@@ -19,7 +19,7 @@ int main(void)
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
-
+     printf("String:[%s]\n", "I am a string !");
     return (0);
 }
 
